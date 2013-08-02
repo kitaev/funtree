2f87ea3fa8e32ac8ae77fe243f3a697c03541832
Alexander Kitaev <kitaev@gmail.com>
2013-08-02 15:54:31 +0200

    WIP: tree strategy introduced

--- src/main/scala/rtree/Tree2.scala
+++ src/main/scala/rtree/Tree2.scala
@@ -1,21 +1,21 @@
 package rtree
 
-class Tree2 {
-  
-  var root: Node2 = createNode(new Array[WithShape](0))
+class Tree2(val strategy: TreeStrategy2) extends TreeStrategy2 {
 
-  def insert(leaf: WithShape): Node2 = {
+  var root: Node2 = createNode(new Array[WithPayload](0))
+
+  def insert(leaf: WithPayload): Node2 = {
     root.insert(leaf) match {
       case Some(roots) => {
         val seq = roots.map(createNode(_))
         root = createNode(seq)
-        root
       }
-      case None => root
+      case None => 
     }
+    root
   }
 
-  def createNode(children: Seq[_ <: WithShape]): Node2 = {
+  def createNode(children: Seq[_ <: WithPayload]): Node2 = {
     if (children.isEmpty || children.find(_.isInstanceOf[Node2]) == None) {
       new LeafNode(this, children)
     } else {
@@ -23,44 +23,58 @@ class Tree2 {
     }
   }
 
-  def split(nodes: Seq[_ <: WithShape]): Option[Seq[Seq[_ <: WithShape]]] = {
-    None
+  def split(nodes: Seq[_ <: WithPayload]): Option[Seq[Seq[_ <: WithPayload]]] = {
+    strategy.split(nodes)
   }
 
-  def findTarget(shape: Shape, targets: Seq[_ <: WithShape]): Int = {
-    0
+  def findTarget(payload: Payload, targets: Seq[_ <: WithPayload]): Int = {
+    strategy.findTarget(payload, targets)
+  }
+  
+  def combinePayload(nodes: Seq[_ <: WithPayload]) : Payload = {
+    strategy.combinePayload(nodes)
+    //nodes.foldLeft[Option[Payload]](None)((acc, c) => Some(c.payload + acc.orNull)).orNull
   }
 }
 
-trait WithShape {
-  def shape: Shape
+trait TreeStrategy2 {
+  def split(nodes: Seq[_ <: WithPayload]): Option[Seq[Seq[_ <: WithPayload]]]
+  def findTarget(payload: Payload, targets: Seq[_ <: WithPayload]): Int
+  def combinePayload(nodes: Seq[_ <: WithPayload]) : Payload
+  
+}
+
+trait Payload
+
+trait WithPayload {
+  def payload: Payload
 }
 
 class EmptyNodeException extends Exception
 
-abstract class Node2(val tree: Tree2, var children: Seq[_ <: WithShape] = new Array[WithShape](0)) extends WithShape {
-  def insert(l: WithShape): Option[Seq[Seq[_ <: WithShape]]]
+abstract class Node2(val tree: Tree2, var children: Seq[_ <: WithPayload] = new Array[WithPayload](0)) extends WithPayload {
+  def insert(l: WithPayload): Option[Seq[Seq[_ <: WithPayload]]]
 
-  def shape: Shape = {
+  def payload: Payload = {
     if (children.isEmpty) {
       throw new EmptyNodeException
     }
-    children.foldLeft[Option[Shape]](None)((acc, c) => Some(c.shape + acc.orNull)).orNull
+    tree.combinePayload(children);    
   }
 }
 
-class LeafNode(tree: Tree2, readyChildren: Seq[_ <: WithShape] = new Array[WithShape](0)) extends Node2(tree, readyChildren) {
+class LeafNode(tree: Tree2, readyChildren: Seq[_ <: WithPayload] = new Array[WithPayload](0)) extends Node2(tree, readyChildren) {
 
-  def insert(l: WithShape): Option[Seq[Seq[_ <: WithShape]]] = {
+  def insert(l: WithPayload): Option[Seq[Seq[_ <: WithPayload]]] = {
     children = children :+ l
     tree.split(children)
   }
 }
 
-class ContainerNode2(tree: Tree2, readyChildren: Seq[_ <: WithShape] = new Array[WithShape](0)) extends Node2(tree, readyChildren) {
+class ContainerNode2(tree: Tree2, readyChildren: Seq[_ <: WithPayload] = new Array[WithPayload](0)) extends Node2(tree, readyChildren) {
 
-  override def insert(l: WithShape): Option[Seq[Seq[_ <: WithShape]]] = {
-    val targetIndex = tree.findTarget(l.shape, children)
+  override def insert(l: WithPayload): Option[Seq[Seq[_ <: WithPayload]]] = {
+    val targetIndex = tree.findTarget(l.payload, children)
 
     val replacement = children(targetIndex) match {
       case n: Node2 => n.insert(l)
